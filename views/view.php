da77df2ce32e29bbd5e456d8e694519d3b95e44b
Séverine Noël <severine.noel94@gmail.com>
2017-09-20 21:56:52 +0200

    integration silex

--- views/view.php
+++ views/view.php
@@ -13,12 +13,12 @@
 <?php
 foreach ( $articles as $article ): ?>
 	<article>
-		<h2><?php echo $article['art_title'] ?></h2>
-		<p><?php echo $article['art_content'] ?></p>
+		<h2><?php echo $article->getTitle() ?></h2>
+		<p><?php echo $article->getContent() ?></p>
 	</article>
 <?php endforeach ?>
 <footer class="footer">
-	<p>Septembre 2017 - Iteration 3</p>
+	<p>Septembre 2017 - Iteration 4</p>
 </footer>
 </body>
 </html>
