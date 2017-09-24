b7f0b7e390428121f3aa59dba7d59a21aa8acffd
Séverine Noël <severine.noel94@gmail.com>
2017-09-24 20:43:17 +0200

    create file

--- src/Domain/Comment.php
+++ src/Domain/Comment.php
@@ -1,7 +1,73 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: severine
- * Date: 21/09/17
- * Time: 22:56
- */
+
+namespace kindcms\Domain;
+
+class Comment {
+	/**
+	 * Comment id.
+	 *
+	 * @var integer
+	 */
+	private $id;
+
+	/**
+	 * Comment author.
+	 *
+	 * @var string
+	 */
+	private $author;
+
+	/**
+	 * Comment content.
+	 *
+	 * @var integer
+	 */
+	private $content;
+
+	/**
+	 * Associated article.
+	 *
+	 * @var \kindcms\Domain\Article
+	 */
+	private $article;
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setId( $id ) {
+		$this->id = $id;
+
+		return $this;
+	}
+
+	public function getAuthor() {
+		return $this->author;
+	}
+
+	public function setAuthor( $author ) {
+		$this->author = $author;
+
+		return $this;
+	}
+
+	public function getContent() {
+		return $this->content;
+	}
+
+	public function setContent( $content ) {
+		$this->content = $content;
+
+		return $this;
+	}
+
+	public function getArticle() {
+		return $this->article;
+	}
+
+	public function setArticle( Article $article ) {
+		$this->article = $article;
+
+		return $this;
+	}
+}
