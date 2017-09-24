c28f8c1cfce6abcad3372aa224ac0b5f1f840acd
Séverine Noël <severine.noel94@gmail.com>
2017-09-24 19:35:38 +0200

    add public function find id

--- src/DAO/ArticleDAO.php
+++ src/DAO/ArticleDAO.php
@@ -2,27 +2,10 @@
 
 namespace kindcms\DAO;
 
-use Doctrine\DBAL\Connection;
 use kindcms\Domain\Article;
 
-class ArticleDAO
+class ArticleDAO extends DAO
 {
-	/**
-	 * Database connection
-	 *
-	 * @var \Doctrine\DBAL\Connection
-	 */
-	private $db;
-
-	/**
-	 * Constructor
-	 *
-	 * @param \Doctrine\DBAL\Connection The Database connection object
-	 */
-	public function __construct(Connection $db) {
-		$this->db = $db;
-	}
-
 	/**
 	 * Return a list of all articles, sorted by date (most recent first).
 	 *
@@ -30,15 +13,32 @@ class ArticleDAO
 	 */
 	public function findAll() {
 		$sql = 'SELECT * FROM t_article ORDER BY art_id DESC';
-		$result = $this->db->fetchAll($sql);
+		$result = $this->getDb()->fetchAll($sql);
 
 		// Convert query result to an array of domain objects
 		$articles = array();
 		foreach ($result as $row) {
 			$articleId = $row['art_id'];
-			$articles[$articleId] = $this->buildArticle($row);
+			$articles[$articleId] = $this->buildDomainObject($row);
 		}
 		return $articles;
+	}
+
+	/**
+	 * Returns an article matching the supplied id.
+	 *
+	 * @param integer $id
+	 *
+	 * @return \kindcms\Domain\Article|throws an exception if no matching article is found
+	 */
+	public function find($id) {
+		$sql = 'SELECT * FROM t_article WHERE art_id=?';
+		$row = $this->getDb()->fetchAssoc($sql, array($id));
+
+		if ($row)
+			return $this->buildDomainObject($row);
+		else
+			throw new \Exception('No Article matching ID' . $id);
 	}
 
 	/**
@@ -47,8 +47,7 @@ class ArticleDAO
 	 * @param array $row The DB row containing Article data.
 	 * @return \kindcms\Domain\Article
 	 */
-
-	private function buildArticle(array $row) {
+	protected function buildDomainObject(array $row) {
 		$article = new Article();
 		$article->setId($row['art_id']);
 		$article->setTitle($row['art_title']);
